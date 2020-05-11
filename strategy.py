bb807b1ded50e981008d6a8001f98583fcd766d9
sahilp-tech <61248705+sahilp-tech@users.noreply.github.com>
2020-05-11 12:47:45 -0700

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -3,7 +3,7 @@ import backtrader
 class TestStrategy(backtrader.Strategy):
     
     def log(self, txt, dt=None):
-        ''' Logging function fot this strategy'''
+        ''' Logging function for this strategy'''
         dt = dt or self.datas[0].datetime.date(0)
         print('%s, %s' % (dt.isoformat(), txt))
 
